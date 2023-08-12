cec6589cbbfbd1dd510b16c9507231e809f82cdb
Florian Veys <florian.veys@qualifio.com>
2023-08-12 18:40:24 +0200

    feat: build waiter using vite

---
+++ waiter/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, 'index.ts'),
+          name: 'WaiterAssistant',
+          fileName: 'waiter-assistant',
+        },
+      },
+});
