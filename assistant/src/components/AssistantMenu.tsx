be8c4765fcdb8f40ef70d6e1353a01f97064e3c8
florianveys <florianveys@gmail.com>
2023-09-26 13:35:12 +0200

    feat: boot react component assistantMenu in each dom element mentionned

---
+++ assistant/src/components/AssistantMenu.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+export function AssistantMenu() {
+  const [collapse, setCollapse] = useState(true);
+  return <>test</>;
+}
--- assistant/src/main.tsx
+++ assistant/src/main.tsx
@@ -1,12 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { StartupPointProvider } from "./context/StartupPoints.tsx";
+import { SESSION_STARTUP_POINTS_KEY } from "../../shared/constant.ts";
+import { z } from "zod";
+import { StartupPoint$ } from "../../shared/type/StartupPoint.ts";
+import { AssistantMenu } from "./components/AssistantMenu.tsx";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById("groom-assistant")!).render(
-  <React.StrictMode>
-    <StartupPointProvider>
-      <App />
-    </StartupPointProvider>
-  </React.StrictMode>,
-);
+let hasStarted = false;
+function startup() {
+  if (hasStarted) {
+    return;
+  }
+
+  const startupPointsFromSession = sessionStorage.getItem(
+    SESSION_STARTUP_POINTS_KEY,
+  );
+
+  if (startupPointsFromSession) {
+    const startupPointsParsed = z
+      .array(StartupPoint$)
+      .safeParse(JSON.parse(startupPointsFromSession));
+
+    if (startupPointsParsed.success) {
+      const startupPoints = startupPointsParsed.data;
+
+      startupPoints.forEach((startupPoint, index) => {
+        let subject;
+        if (startupPoint.identifier.type === "id") {
+          subject = document.getElementById(startupPoint.identifier.value);
+        }
+
+        if (subject) {
+          const assistantWrapperId = `assistant-${index}`;
+
+          const wrapper = document.createElement("div");
+          wrapper.id = assistantWrapperId;
+          wrapper.className = "assistant-wrapper";
+
+          const assistant = document.createElement("div");
+          assistant.className = "assistant-position-right";
+
+          subject.parentNode?.insertBefore(wrapper, subject);
+          wrapper.appendChild(subject);
+          wrapper.appendChild(assistant);
+
+          ReactDOM.createRoot(assistant).render(
+            <React.StrictMode>
+              <AssistantMenu></AssistantMenu>
+            </React.StrictMode>,
+          );
+        }
+      });
+    }
+  }
+
+  hasStarted = true;
+}
+
+startup();
