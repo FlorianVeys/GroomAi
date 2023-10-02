301dc390c14e05c688e6e9b9a02c076951442289
florianveys <florianveys@gmail.com>
2023-10-02 12:34:57 +0200

    feat: create an openAiService layer

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,19 @@
+import { OpenAI } from 'openai';
 import { Module } from '@nestjs/common';
 import { PrePromptController } from './pre-prompt/pre-prompt.controller';
 import { PrePromptModule } from './pre-prompt/pre-prompt.module';
 import { OpenAiService } from './open-ai/open-ai.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrePromptModule],
+  imports: [ConfigModule.forRoot(), PrePromptModule],
   controllers: [PrePromptController],
-  providers: [OpenAiService],
+  providers: [
+    {
+      provide: OpenAI,
+      useValue: new OpenAI(),
+    },
+    OpenAiService,
+  ],
 })
 export class AppModule {}
--- api/src/open-ai/open-ai.service.ts
+++ api/src/open-ai/open-ai.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 @Injectable()
-export class OpenAiService {}
+export class OpenAiService {
+  constructor(private openAi: OpenAI) {}
+
+  async completion(prompt: string): Promise<string | null> {
+    try {
+      // TODO - Aggregate potential previous messages for reconstruction of context
+      const {
+        choices: [choice],
+      } = await this.openAi.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+        n: 1,
+        temperature: 0.7,
+        max_tokens: 256,
+      });
+
+      return choice.message.content;
+    } catch (e) {
+      // TODO - react to error events accordingly
+      if (e instanceof OpenAI.APIError) {
+        console.error({
+          type: 'OpenAIError',
+          error: e,
+        });
+      } else {
+        console.error({
+          type: 'UnknownError',
+          error: e,
+        });
+      }
+
+      return null;
+    }
+  }
+}
