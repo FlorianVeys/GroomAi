50c0c0343c5b50676381b0d7ed1ecb1e588dd29c
florianveys <florianveys@gmail.com>
2023-10-02 09:42:58 +0200

    feat: boilerplate for openai service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrePromptController } from './pre-prompt/pre-prompt.controller';
 import { PrePromptModule } from './pre-prompt/pre-prompt.module';
+import { OpenAiService } from './open-ai/open-ai.service';
 
 @Module({
   imports: [PrePromptModule],
   controllers: [PrePromptController],
-  providers: [],
+  providers: [OpenAiService],
 })
 export class AppModule {}
