a11811d4058050d0cd9ec0d6ee5c4eb3ff0126e9
florianveys <florianveys@gmail.com>
2023-10-02 09:36:21 +0200

    feat: delete nestjs boilerplate

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrePromptController } from './pre-prompt/pre-prompt.controller';
 import { PrePromptModule } from './pre-prompt/pre-prompt.module';
 
 @Module({
   imports: [PrePromptModule],
-  controllers: [AppController, PrePromptController],
-  providers: [AppService],
+  controllers: [PrePromptController],
+  providers: [],
 })
 export class AppModule {}
